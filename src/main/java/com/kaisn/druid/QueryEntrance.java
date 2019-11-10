8ac9cf0f68acfc9160e14a0aba0e689193e7fe8e
lijing <1169318609@qq.com>
2019-11-11 00:05:31 +0800

    Druid查询优化

--- src/main/java/com/kaisn/druid/QueryEntrance.java
+++ src/main/java/com/kaisn/druid/QueryEntrance.java
@@ -77,21 +77,17 @@ public class QueryEntrance {
     }
 
     private static String postWithJson(String url, String json) {
-        String returnValue = "这是默认返回值，接口调用失败";
+        String returnValue = "request failed.";
         CloseableHttpClient httpClient = HttpClients.createDefault();
         ResponseHandler<String> responseHandler = new BasicResponseHandler();
         try{
-            //第一步：创建HttpClient对象
             httpClient = HttpClients.createDefault();
-            //第二步：创建httpPost对象
             HttpPost httpPost = new HttpPost(url);
-            //第三步：给httpPost设置JSON格式的参数
             StringEntity requestEntity = new StringEntity(json,"utf-8");
             requestEntity.setContentEncoding("UTF-8");
             httpPost.setHeader("Content-type", "application/json");
             httpPost.setEntity(requestEntity);
-            //第四步：发送HttpPost请求，获取返回值
-            returnValue = httpClient.execute(httpPost,responseHandler); //调接口获取返回值时，必须用此方法
+            returnValue = httpClient.execute(httpPost,responseHandler);
         }catch(Exception e){
             e.printStackTrace();
         }finally {
@@ -101,7 +97,6 @@ public class QueryEntrance {
                 e.printStackTrace();
             }
         }
-        //第五步：处理返回值
         return returnValue;
     }
 }
