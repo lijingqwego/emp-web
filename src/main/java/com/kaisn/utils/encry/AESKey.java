554cdb321d703ed091cb4fdad26fba17118a611b
lijingqwego <1169318609@qq.com>
2019-11-22 22:11:35 +0800

    代码优化

--- src/main/java/com/kaisn/utils/encry/AESKey.java
+++ src/main/java/com/kaisn/utils/encry/AESKey.java
@@ -1,7 +1,8 @@
 package com.kaisn.utils.encry;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.Base64.Decoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -42,7 +43,8 @@ public class AESKey {
         IvParameterSpec iv = new IvParameterSpec(vector.getBytes());// 使用CBC模式，需要一个向量iv，可增加加密算法的强度
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
         byte[] encrypted = cipher.doFinal(encData.getBytes("utf-8"));
-        return new BASE64Encoder().encode(encrypted);// 此处使用BASE64做转码。
+        Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(encrypted);// 此处使用BASE64做转码。
     }
 
     // 加密
@@ -54,7 +56,8 @@ public class AESKey {
             IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());// 使用CBC模式，需要一个向量iv，可增加加密算法的强度
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
             byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
-            return new BASE64Encoder().encode(encrypted);// 此处使用BASE64做转码。
+            Encoder encoder = Base64.getEncoder();
+            return encoder.encodeToString(encrypted);// 此处使用BASE64做转码。
         } catch (Exception e) {
             return null;
         }
@@ -68,7 +71,8 @@ public class AESKey {
             IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());// 使用CBC模式，需要一个向量iv，可增加加密算法的强度
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
             byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
-            return new BASE64Encoder().encode(encrypted);// 此处使用BASE64做转码。
+            Encoder encoder = Base64.getEncoder();
+            return encoder.encodeToString(encrypted);// 此处使用BASE64做转码。
         } catch (Exception e) {
             return null;
         }
@@ -82,7 +86,8 @@ public class AESKey {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-            byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);// 先用base64解密
+            Decoder decoder = Base64.getDecoder();
+            byte[] encrypted1 = decoder.decode(sSrc);// 先用base64解密
             byte[] original = cipher.doFinal(encrypted1);
             String originalString = new String(original, "utf-8");
             return originalString;
@@ -99,7 +104,8 @@ public class AESKey {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-            byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);// 先用base64解密
+            Decoder decoder = Base64.getDecoder();
+            byte[] encrypted1 = decoder.decode(sSrc);// 先用base64解密
             byte[] original = cipher.doFinal(encrypted1);
             String originalString = new String(original, "utf-8");
             return originalString;
@@ -115,7 +121,8 @@ public class AESKey {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             IvParameterSpec iv = new IvParameterSpec(ivs.getBytes());
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-            byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);// 先用base64解密
+            Decoder decoder = Base64.getDecoder();
+            byte[] encrypted1 = decoder.decode(sSrc);// 先用base64解密
             byte[] original = cipher.doFinal(encrypted1);
             String originalString = new String(original, "utf-8");
             return originalString;
