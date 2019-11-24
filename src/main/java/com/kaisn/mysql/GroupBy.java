5a600f578bd3e5e9ef92fd17583606d298fe64e8
lijingqwego <1169318609@qq.com>
2019-11-24 11:44:12 +0800

    sql query

--- src/main/java/com/kaisn/mysql/GroupBy.java
+++ src/main/java/com/kaisn/mysql/GroupBy.java
@@ -3,4 +3,20 @@ package com.kaisn.mysql;
 public class GroupBy {
     private String column;
     private String sort;
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
 }
